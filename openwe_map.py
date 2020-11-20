da16dcdb05fc4f8fb059602b7db7422716be9163
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-11-20 18:23:08 +0300

    owm weather functions

--- openwe_map.py
+++ openwe_map.py
@@ -10,3 +10,11 @@ w=place.weather
 temp=w.temperature('celsius')
 
 print(temp)
+
+# w.detailed_status         # 'clouds'
+# w.wind()                  # {'speed': 4.6, 'deg': 330}
+# w.humidity                # 87
+# w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+# w.rain                    # {}
+# w.heat_index              # None
+# w.clouds                  # 75
