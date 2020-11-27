2f853981b34812af0bfd468396d283a682ad1b3e
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-11-27 14:41:18 +0300

    qrcode-cv2 v2.0

--- qrcode_cv2.py
+++ qrcode_cv2.py
@@ -1,7 +1,11 @@
 from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import Decoded
 import cv2
 import os
 import pprint
 
 wifi_info=decode(cv2.imread(os.path.curdir+'/test_images/wifi.jpeg'))
-pprint.pprint(wifi_info)
+
+print(wifi_info)
+for info in wifi_info:
+     pprint.pprint(info)
